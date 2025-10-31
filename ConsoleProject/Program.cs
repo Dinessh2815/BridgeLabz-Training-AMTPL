a1e2ae98036b619e08ae9a715b468617d6f3a058
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-10-31 17:56:04 +0530

    Initial commit

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace ConsoleProject
+﻿
+
+using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
-}
+}   
