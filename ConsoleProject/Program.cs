5c1e46decd6bd20589d80e1c8fe52b64b74d9d6a
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-01 13:07:51 +0530

    Practiced - I/O, Data types, Conversion _2

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -1,11 +1,22 @@
 ﻿
 
 using System;
-
-class Program
+namespace myFirstProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Student name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Student age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Fee");
+            double fee = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine($"Hi {name}! your age is: {age}. \n The fee student paying is: {fee}");
+        }
     }
-}   
+}
