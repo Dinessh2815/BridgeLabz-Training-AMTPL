72449eee13f2efccbe01f07a88e44abad0822bf7
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-03 15:44:54 +0530

    implemented 1.coin flip problem

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -4,17 +4,40 @@
     {
         static void Main()
         {
-            int[] arr = new int[5];
-            arr[0] = 10;
-            arr[1] = 20;
-            arr[2] = 30;
-            arr[3] = 40;
-            arr[4] = 50;
-
-            for(int i = 0; i <= 5; i++)
+            int flips;
+            Console.WriteLine("Enter the number of flips of the coin");
+            flips = Convert.ToInt32(Console.ReadLine());
+            if (flips < 0)
             {
-                Console.WriteLine(arr[i]);
+                Console.WriteLine("Error: Invalid Number of flips entered!");
+            }
+            else
+            {
+                int headCount = 0;
+                int tailCount = 0;
+
+                Random random = new Random();
+
+                for(int i = 0; i < flips; i++)
+                {
+                    double result = random.NextDouble();
+                    if (result > 0.5)
+                    {
+                        headCount++;
+                    }
+                    else
+                    {
+                        tailCount++;
+                    }
+                }
+                double tailPercentage = (double) tailCount / flips * 100;
+                double headPercentage = (double) headCount / flips * 100;
+
+                Console.WriteLine($"The number of flips are {flips}");
+                Console.WriteLine($"the probability of tails is {tailPercentage}%");
+                Console.WriteLine($"the probability of tails is {headPercentage}%");
+
+            }
             }
         }
-    }
 }
