6f410f4159f7b0099cf1a27a07cfdaf826ab8ee8
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-03 15:13:27 +0530

    basic array implementation

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace ArraysProject
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = new int[5];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+            arr[4] = 50;
+
+            for(int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
         }
     }
 }
