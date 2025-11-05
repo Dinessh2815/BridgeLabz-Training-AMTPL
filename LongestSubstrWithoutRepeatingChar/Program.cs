16ecb3d31d1037c59611454f4c59848f20b17a3c
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-05 16:44:27 +0530

    Completed Longest Substring without repeating character

---
+++ LongestSubstrWithoutRepeatingChar/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LongestSubstrWithoutRepeatingChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcabcbb";
+            int left = 0;
+            int right = 0;
+            int maxLen = 0;
+            while (right < s.Length)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    if (s[i] == s[right])
+                    {
+                        left = i + 1;
+                        break;
+                    }
+
+                }
+                if(maxLen < right - left + 1)
+                {
+                    maxLen = right - left + 1;
+                }
+                    right++;
+
+            }
+            Console.Write(maxLen);
+        }
+    }
+}
