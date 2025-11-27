67adc550ae0d96058a89050b2e51dbf8acb8be05
Dinessh2815 <dinessh.venkat28@gmail.com>
2025-11-27 15:41:19 +0530

    Collections

---
+++ Generic/LongestConsecutive.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject
+{
+    internal class LongestConsecutive
+    {
+
+public class LongestConsecutiveSeq
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 100, 4, 200, 1, 3, 2 };
+
+            Console.WriteLine("Longest Consecutive Length = " + LongestConsecutive(nums));
+        }
+
+        public static int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            HashSet<int> set = new HashSet<int>(nums);
+            int longest = 0;
+
+            foreach (int num in set)
+            {
+                if (!set.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int count = 1;
+
+
+                    while (set.Contains(currentNum + 1))
+                    {
+                        currentNum++;
+                        count++;
+                    }
+
+                    longest = Math.Max(longest, count);
+                }
+            }
+
+            return longest;
+        }
+    }
+
+}
+}
---
+++ ImportantMethods/ImportantMethods/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ImportantMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
